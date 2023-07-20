395263b3f9e4e836117cb98792cb465ba839e9fe
Yahia Ahmed <yahiaeltouny1980@gmail.com>
2023-07-19 17:05:29 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	return (0);
+}
